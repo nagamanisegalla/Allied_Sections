ff70081e42c6c221cdfb06b83c6c5b9b1263661d
nagamanisegalla <nagamanisegalla@gmail.com>
2026-04-11 15:49:22 +0530

    msg

---
+++ Javascript/new.js
@@ -0,0 +1,35 @@
+// const arr1 = [1,2,3,4]
+// const arr2 = [...arr1,5,6]
+// console.log(arr2)
+// const obj1 = {
+//     name : "naga",
+//     sub : "fsd"
+// }
+// const obj2 = {
+//     ...obj1,
+//     roll : 42
+// }
+// console.log(obj2)
+// function greet(...num){
+//     console.log(num.reduce((a,b) => a+b))
+// }
+// greet(10,20,30,40,50,60)
+// const arr1 = [1,2,3]
+// const [a,...b] = arr1
+// console.log(a)
+// console.log(b)
+// const obj2 = {
+//     name : "naga",
+//     roll : 24
+// }
+// const {name : n,roll : r} = obj2
+// console.log(n)
+// console.log(r)
+
+
+// console.log(a)
+// let a= 10
+greet()
+function greet(){
+    console.log("welcome")
+}

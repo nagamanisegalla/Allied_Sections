697a82b90b82f54ef46a5e7ab6e43cffde6e169a
nagamanisegalla <nagamanisegalla@gmail.com>
2026-04-11 15:41:24 +0530

    commit

---
+++ Javascript/new.js
@@ -0,0 +1,37 @@
+// const arr1 = [1,2,3]
+// const arr2 = [...arr1,4,5,6]
+// console.log(arr2)
+// const obj = {
+//     name : "naga",
+//     sub : "node"
+// }
+// const obj2 = {
+//     ...obj,
+//     course : "ai"
+// }
+// console.log(obj2)
+// function array1(...num){
+//     console.log(num.reduce((a,b) => a+b))
+// }
+// array1(2,3,4,5,6,7)
+
+// const arr1 = [1,2,3]
+// const [a,...b] = arr1
+// console.log(a)
+// console.log(b)
+// const obj1 = {
+//     name : "jeevitha",
+//     roll : 24
+// }
+// const {name : n,roll : r} = obj1
+// console.log(n)
+// console.log(r)
+
+
+// console.log(a)
+// const a =10
+greet()
+function greet(){
+    console.log("hello")
+}
+
---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+const a = 10
+console.log(a)
+{
+    const a = 20
+    console.log(a)  
+}
+
+console.log(a)  
+
